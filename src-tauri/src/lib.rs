16452edef2d20389a754c7734f762854bdbe062e
fteychene <francois.teychene@gmail.com>
2025-01-15 09:59:14 +0100

    feat: Add copy to clipboard result

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -94,6 +94,7 @@ pub fn run() {
     let path = env::current_dir().unwrap();
     println!("The current directory is {}", path.display());
     tauri::Builder::default()
+        .plugin(tauri_plugin_clipboard_manager::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![
--- src/Result.tsx
+++ src/Result.tsx
@@ -1,4 +1,7 @@
+import Icon from "@mdi/react";
 import { RollResult } from "./model.ts"
+import { writeText } from '@tauri-apps/plugin-clipboard-manager';
+import { mdiContentCopy } from '@mdi/js';
 
 interface ResultProps {
     result?: RollResult
@@ -6,7 +9,16 @@ interface ResultProps {
 
 function Result({ result }: ResultProps) {
     if (result) {
-        return <div className="notification is-primary is-light py-2 my-3">Rolled {result.skill ? result.skill : "generic"} ={">"} {result.result} [{result.rolls.join(", ")}]</div>;
+        const resultText = `Rolled ${result.skill ? result.skill : "generic"} => ${result.result} [${result.rolls.join(", ")}]`;
+        return (
+            <div className="notification is-primary is-light py-2 my-3 level">
+                <div className="level-left">
+                    <p className="level-item">{resultText}</p>
+                </div>
+                <div className="level-right">
+                    <p className="level-item"><a className="button" onClick={() => writeText(resultText).catch(e => {console.error(e); alert("Can't copy to clipboard. See console for details");})}><Icon path={mdiContentCopy} size={1} /></a></p>
+                </div>
+            </div>);
     }
     return null;
 }
